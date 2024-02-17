215868bda411e713e6ae207e58b64280755380d6
WiseMrMusa <abdulkareemusa01@gmail.com>
2024-02-17 17:37:42 +0100

    changed chain checking to starknet-react

--- packages/connectkit-strk/src/hooks/useChainIsSupported.ts
+++ packages/connectkit-strk/src/hooks/useChainIsSupported.ts
@@ -1,7 +1,8 @@
 import { useConfig } from 'wagmi';
+import { useStarknet } from '@starknet-react/core/src/context/starknet'
 
 export function useChainIsSupported(chainId?: number): boolean | null {
-  const { chains } = useConfig();
+  const { chains } = useStarknet();
   if (!chainId) return false;
-  return chains.some((x) => x.id === chainId);
+  return chains.some((x) => Number(x.id) === chainId);
 }
